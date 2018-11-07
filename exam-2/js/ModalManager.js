92db36b223904cd58647067f34c4aab87c97c245
Benjamin Streit <benjaminmstreit@gmail.com>
2018-11-07 12:45:24 -0600

    Importing.

---
+++ exam-2/js/ModalManager.js
@@ -0,0 +1,18 @@
+/**
+  * Class for managing all modals across pages
+*/
+class ModalManager {
+  /**
+    * Constructor
+  */
+  constructor() {
+
+  }
+
+  /**
+    * Performs operations to present home modal
+  */
+  operationHome(operation) {
+    $('#modal_home').modal('show');
+  }
+}
